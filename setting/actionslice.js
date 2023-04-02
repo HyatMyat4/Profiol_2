7f084a6a1108939a39384470f0b18b1d7a36c2b4
HyatMyat <rapperlay2584@gmail.com>
2023-04-02 20:13:06 +0630

    give star

--- setting/actionslice.js
+++ setting/actionslice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = { 
     darkmode : true ,
-    starmode : false
+    starmode : true
 }
 export const Actionslice = createSlice({
     name: 'actionslice',
