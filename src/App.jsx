56aa600aeb6d67179d96f8e3825f3b7c78be5015
HyatMyat <rapperlay2584@gmail.com>
2023-05-01 13:33:50 +0630

    addMessanger

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import Main from "./components/Main";
 import LeftMain from "./components/LeftMain";
 import RightMain from "./components/RightMain";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   const darklight = useSelector(darkmodeC);
@@ -22,6 +23,10 @@ function App() {
         <Main />
         <RightMain />
       </div>
+         <MessengerCustomerChat
+            pageId="113485224987738"
+            appId="735689564765924"            
+        />
     </div>
   );
 }
